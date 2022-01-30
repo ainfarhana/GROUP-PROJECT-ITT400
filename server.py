78e0b4b63924a15fd1ac9252fc6f6a52b90530a5
IyliRifhani <93872946+IyliRifhani@users.noreply.github.com>
2022-01-30 21:38:35 +0800

    second commit

--- server.py
+++ server.py
@@ -10,3 +10,23 @@ port = 8080
 new_socket.bind(("", port))
 print("Request has been successful!")
 print("This is your IP: ", '192.168.56.102')
+
+name = input('Enter name: ')
+
+new_socket.listen(1)
+
+
+conn, add = new_socket.accept()
+
+print("Received connection from ", add[0])
+print('Connection Established. Connected From: ',add[0])
+
+client = (conn.recv(1024)).decode()
+print(client + ' has joined...')
+conn.send(name.encode())
+while True:
+    message = input('Me : ')
+    conn.send(message.encode())
+    message = conn.recv(1024)
+    message = message.decode()
+    print(client, ':', message)
